d97ab147f391547ca45f2cd1f0308b619c208e58
mrinalsanskar <37977161+mrinalsanskar@users.noreply.github.com>
2020-05-13 00:07:28 +0530

    Add files via upload

---
+++ AuthenticateDnd/src/main/java/com/cg/dnd/AuthenticateDndApplication.java
@@ -0,0 +1,13 @@
+package com.cg.dnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticateDndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticateDndApplication.class, args);
+	}
+
+}
